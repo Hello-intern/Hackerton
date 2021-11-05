62b28b5cafb7210f081c18d7bad6d9e091d62d24
everglow <78077569+okchanho@users.noreply.github.com>
2021-11-05 15:50:07 +0900

    first commit

--- Hackerton/src/main/java/com/example/hackerton/HackertonApplication.java
+++ Hackerton/src/main/java/com/example/hackerton/HackertonApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HackertonApplication {
-
+    /*된건가*/
     public static void main(String[] args) {
         SpringApplication.run(HackertonApplication.class, args);
     }
